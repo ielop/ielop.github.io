8ca98b6ea833347955d4d89f8e3f79335f77465c
Suilan Maia <suilan.maia@vale.com>
2017-02-14 16:59:03 -0300

    change subtitle logo

--- js/canvas.js
+++ js/canvas.js
@@ -19,29 +19,35 @@ function makeCircles() {
         var circle,
         newX = Math.random()>0.5?window.innerWidth:0,
         newY = Math.random()>0.5?window.innerHeight:0,
-        random = Math.random()*5;
+        dX,dY,
+        random = Math.random()*4;
         circle = {};
         // Start render outside vision canvas
+
         if(newY>0){
             newY = newY-dimensions[0]*random;
+            dY="up";
         }
         else {
             newY = newY+dimensions[0]*random;
+            dY="down";
         }
 
         if(newX>0){
             newX = newX-dimensions[0]*random;
+            dX="left";
         }
         else {
             newX = newX+dimensions[0]*random;
+            dX="right";
         }
 
-        console.log("X:"+newX);
-        console.log("Y:"+newY);
         for (var i = 0; i < 3; i++) {
             circle[i] ={
                 posX: newX,
                 posY: newY,
+                directionX:dX,
+                directionY:dY,
                 color: circleColors[i],
                 radious:dimensions[i]*random
             };
@@ -63,24 +69,64 @@ function makeCircles() {
     } //end function renderContent
 
     function animationLoop() {
-        canvas.width = canvas.width,
+        var c;
+        canvas.width = canvas.width;
         speed = 1;
         renderContent();
         for (var i = 0; i < circles.length; i++) {
             // Invert moviment
             for (var j = 0; j < 3; j++) {
-                var c = circles[i][j];
-                if(c.posY>=-c.radious) c.posY -= speed;
-                else c.posY += speed;
+                c = circles[i][j];
 
-                if(c.posX>=-c.radious) c.posX -= speed;
-                else c.posX += speed;
+                if( c.directionY=="up" ){
+                    c.posY -= speed;
+                }
+                else{
+                    c.posY += speed;
+                }
+
+                if( c.directionX=="left" ){
+                    c.posX -= speed;
+                }
+                else{
+                    c.posX += speed;
+                }
             }
+            c=circles[i][0];
+
+            if(c.posY<=c.radious && c.directionY=="up") {
+                circles[i][0].directionY="down";
+                circles[i][1].directionY="down";
+                circles[i][2].directionY="down";
+            }
+            else if( c.posY>=(window.innerHeight-c.radious) && c.directionY=="down" ) {
+                // console.log("Y:"+c.posY)
+                // console.log("R:"+c.radious)
+                circles[i][0].directionY="up";
+                circles[i][1].directionY="up";
+                circles[i][2].directionY="up";
+            }
+
+            if(c.posX<=c.radious && c.directionX=="left") {
+                // console.log("X:"+c.posX)
+                // console.log("R:"+c.radious)
+                circles[i][0].directionX="right";
+                circles[i][1].directionX="right";
+                circles[i][2].directionX="right";
+            }
+            else if( c.posX>=(window.innerWidth-c.radious) && c.directionX=="right" ) {
+                // console.log("X:"+c.posX)
+                // console.log("R:"+c.radious)
+                circles[i][0].directionX="left";
+                circles[i][1].directionX="left";
+                circles[i][2].directionX="left";
+            }
+
         }
         setTimeout(animationLoop, 33);
     } //end function animationLoop
 
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 4; i++) {
         defineCircle();
     }
 
